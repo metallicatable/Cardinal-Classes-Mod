bca329c86d50a5a27c3672e0093eaa7a37ba802c
WildYGuy <wildwyguy@gmail.com>
2026-03-06 23:28:44 -0500

    added scrolls (no sprites yet sadge)

---
+++ Content/Items/Weapons/Heretic/MacabreScroll.cs
@@ -0,0 +1,71 @@
+﻿using fourClassesMod.Common.Classes.Heretic; 
+using Microsoft.Xna.Framework; 
+using Steamworks; 
+using Terraria; 
+using Terraria.DataStructures; 
+using Terraria.ID; 
+using Terraria.Localization; 
+using Terraria.ModLoader; 
+using fourClassesMod.Common; 
+ 
+namespace fourClassesMod.Content.Items.Weapons.Heretic 
+{
+    internal class MacabreScroll : ModItem
+    {
+
+        private int lifeCost; // Add our custom resource cost 
+        private string deathMessage;
+        private int messageHelper;
+
+        public override string Texture => $"fourClassesMod/Sprites/Weapons/Slime_Rain";
+
+        public override void SetDefaults()
+        {
+            Item.damage = 15;
+            Item.knockBack = 4f;
+            Item.useStyle = ItemUseStyleID.Shoot; // Makes the player do the proper arm motion 
+            Item.useAnimation = 60;
+            Item.useTime = 60;
+            Item.width = 32;
+            Item.height = 32;
+            Item.UseSound = SoundID.Item1;
+            Item.DamageType = ModContent.GetInstance<HereticDamageClass>();
+            Item.noMelee = true; // The projectile will do the damage and not the item 
+            lifeCost = 5;
+            Item.rare = ItemRarityID.White;
+            Item.shoot = ProjectileID.PurificationPowder;
+
+            Item.shootSpeed = 8f; // This value bleeds into the behavior of the projectile as velocity, keep that in mind when tweaking values 
+        }
+
+        // Make sure you can't use the item if you don't have enough resource 
+
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) 
+        {
+            float numberProjectiles = 5; 
+            float rotation = MathHelper.ToRadians(15); 
+            type = ProjectileID.TopazBolt;
+            player.Hurt(PlayerDeathReason.ByCustomReason(NetworkText.FromKey(ModContent.GetInstance<DeathMessagesPlayer>().hereticDeathMessages)), lifeCost, 0, false, false, -1, false, 500, 500, 0f); 
+
+            position += Vector2.Normalize(velocity) * 45f; 
+
+            for (int i = 0; i < numberProjectiles; i++) 
+            {
+                Vector2 perturbedSpeed = velocity.RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (numberProjectiles - 1))); // Watch out for dividing by 0 if there is only 1 projectile.
+                Projectile.NewProjectile(source, position, perturbedSpeed, type, damage, knockback, player.whoAmI);
+            }
+
+            return false; // return false to stop vanilla from calling Projectile.NewProjectile.
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.EbonstoneBlock, 40)
+                .AddIngredient(ItemID.Silk, 8)
+                .AddTile(TileID.Loom)
+                .Register();
+        }
+    }
+}
