7ddd36d575e3d54fd9019dbfed37456a9302acee
metallicatable <153137415+metallicatable@users.noreply.github.com>
2026-03-12 19:18:09 -0400

    fixed heretic emblem

---
+++ Content/Items/Accessories/Heretic/HereticEmblem.cs
@@ -0,0 +1,40 @@
+﻿using fourClassesMod.Common.Classes.Heretic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace fourClassesMod.Content.Items.Accessories.Heretic 
+{
+    internal class HereticEmblem : ModItem 
+    {
+
+        public override string Texture => $"fourClassesMod/Sprites/Accessories/HereticEmblem"; 
+        
+
+        public override void SetDefaults()
+        {
+            Item.accessory = true;
+            Item.rare = ItemRarityID.LightRed; 
+        }
+
+
+
+
+        public override void UpdateAccessory(Player player, bool hideVisual) 
+        {
+            
+            
+            player.GetDamage<HereticDamageClass>() += 0.15f; 
+
+           
+
+        }
+
+
+    }
+}
